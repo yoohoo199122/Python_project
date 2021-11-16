6e8cb0df9d08d526cb28228c384d00df0a0143e6
Tikhonenko_Igor <yoohoo199122@mail.ru>
2021-11-16 23:04:11 +0300

    Урок №2 - задание №2

---
+++ Task_2.py
@@ -0,0 +1,16 @@
+my_list = ['a', 'b', 'c', 'd', 'e']
+if len(my_list) % 2 == 0:
+    i = 0
+    while i < len(my_list):
+        el = my_list[i]
+        my_list[i] = my_list[i+1]
+        my_list[i+1] = el
+        i += 2
+else:
+    i = 0
+    while i < len(my_list) - 1:
+        el = my_list[i]
+        my_list[i] = my_list[i + 1]
+        my_list[i + 1] = el
+        i += 2
+print(my_list)
