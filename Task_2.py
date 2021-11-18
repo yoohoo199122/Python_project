3c863c2fd36274a5b84edd427dccc48944f0492c
Tikhonenko_Igor <yoohoo199122@mail.ru>
2021-11-18 20:51:54 +0300

    Урок №3 - заданиe №2

---
+++ Task_2.py
@@ -0,0 +1,6 @@
+def my_func(name, surname, year, city, email, telephone):
+    return ([name, surname, year, city, email, telephone])
+
+
+print(my_func(surname='Tikhonenko', name='Igor', year='1991', city='Spb', email='coolcar@mail.ru',
+              telephone='+7-914-470-95-23'))
