12fd0b30deefed95b5554e04019723a4b4806143
Tikhonenko_Igor <yoohoo199122@mail.ru>
2021-11-19 21:47:14 +0300

    Урок №3 - заданиe №5

---
+++ Task_6.py
@@ -0,0 +1,10 @@
+def my_func(a):
+    separate_word = a.split(' ')
+    total = []
+    for i in separate_word:
+        string_element = str(i)
+        first_letter = string_element[:1].upper()
+        word = first_letter + string_element[1:]
+        total.append(word)
+    return total
+print(my_func("hello world"))
